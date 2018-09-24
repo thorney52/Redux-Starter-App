c11df0dac314898e40c1489d2f3c8987b41bd23a
dave <djthorne@edu.uwaterloo.ca>
2018-09-24 17:15:14 -0400

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Javascript: The good parts'},
+        { title: 'Harry Potter'},
+        { title: 'The Dark Tower'},
+        { title: 'Eloquent Ruby'}
+    ];
+}
+
